ca498188fdaee2890b4c58d25c1f3271799a4621
MartinDHM <martin.duhem1@gmail.com>
2023-10-16 10:07:35 +0200

    correction position header

--- Frontend/argentbankapp/src/components/Headers.js
+++ Frontend/argentbankapp/src/components/Headers.js
@@ -1,16 +1,18 @@
 import React from "react";
 import logo from "../assets/images/argentBankLogo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { signOut } from "../Redux/store";
 
 function Headers() {
   const isLoggedIn = useSelector((state) => state.signIn.token !== "");
   const dispatch = useDispatch();
-  const userData = useSelector((state) => state.userProfile); // Utilisez userProfile pour accéder aux données utilisateur
+  const userData = useSelector((state) => state.userProfile);
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(signOut());
+    navigate("/");
   };
 
   return (
@@ -23,15 +25,19 @@ function Headers() {
             alt="Argent Bank Logo"
           />
         </Link>
-        <div>
+        <div className="Username-position">
+          {isLoggedIn ? (
+            <div>
+              <i className="usericon fa fa-user-circle "></i>
+
+              {userData
+                ? userData.userName // Accédez directement à userName
+                : ""}
+            </div>
+          ) : null}
           {isLoggedIn ? (
             <Link to="/" className="main-nav-item">
               <div>
-                <i className="usericon fa fa-user-circle "></i>
-
-                {userData
-                  ? `${userData.userName}` // Accédez directement à userName
-                  : ""}
                 <button className="logbutton" onClick={handleLogout}>
                   <i className="fa fa-sign-out"></i> Sign out
                 </button>
