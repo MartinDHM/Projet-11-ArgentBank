d99b36220569291cb3363c1d6c1e7d4df3a1201d
MartinDHM <martin.duhem1@gmail.com>
2023-10-17 16:03:01 +0200

    update image page 404

--- Frontend/argentbankapp/src/components/Error/Error.js
+++ Frontend/argentbankapp/src/components/Error/Error.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Img404 from "../../assets/images/404.webp";
 
 // Composant NotFound
 export default function NotFound() {
@@ -8,7 +9,7 @@ export default function NotFound() {
       <>
         <div className="notfound-container">
           {/* Titre de la page 404 */}
-          <h1 className="notfound-title">404</h1>
+          <img src={Img404} alt="Error 404" />
           {/* Message d'erreur */}
           <p className="notfound-oups">Oups ! Une erreur est survenue.</p>
           {/* Lien de retour vers la page d'accueil */}
